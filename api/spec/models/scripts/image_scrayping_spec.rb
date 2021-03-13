249b370edbfab5b8be07bec980cd0fde08613f68
ahgowpc <arduino0202@gmail.com>
2021-03-13 21:45:24 +0900

    ImageScraypingSpec 側作成

---
+++ api/spec/models/scripts/image_scrayping_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Scripts::ImageScrayping, type: :model do
+  describe 'methods' do
+    describe '#get_image' do
+      it 'is correct' do
+        expect(true).to eq true
+      end
+    end
+  end
+end
